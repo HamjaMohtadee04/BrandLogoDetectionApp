ec2bed454eb512242100d55550dcdf2eb8fefcab
Hamza Mohtadi <hamzamohtadi225@gmail.com>
2023-02-01 13:08:43 +0600

    Register activity done.Unique name varificatin,datepicker,mobile verification,gmail varification also done

---
+++ app/src/main/java/com/example/brandlogodetectionapp/ReadWriteUserDetails.java
@@ -0,0 +1,12 @@
+package com.example.brandlogodetectionapp;
+
+public class ReadWriteUserDetails {
+    public String doB,gender,mobile;
+
+public ReadWriteUserDetails(String textDoB,String textGender,String textMobile){
+
+    this.doB=textDoB;
+    this.mobile=textMobile;
+    this.gender=textGender;
+}
+}
